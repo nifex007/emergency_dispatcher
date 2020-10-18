17ffffc030b3ab6eba183232a8a4032b6346e944
Nifemi Sola-Ojo <nifex007@yahoo.com>
2020-10-18 11:30:49 +0100

    strapi setup

---
+++ emergency_dispatcher/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48a79024128a6e35f51006f3339eca65'),
+    },
+  },
+});
---
+++ emergency_dispatcher/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ea936a4-4568-4838-94f0-0bd6de3b6682'
+};
