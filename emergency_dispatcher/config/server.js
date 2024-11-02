a3858437247e849865b2651cd3a5f02c725852ea
Nifemi Sola-Ojo <nifex007@yahoo.com>
2024-11-02 06:39:20 +0100

    remove strapi code

--- emergency_dispatcher/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '62aaa84d734feb85786ebc08f53c0d4b'),
-    },
-  },
-});
--- emergency_dispatcher/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '762448a8-84c1-40f2-93fe-4ad60c8da9ef'
-};
